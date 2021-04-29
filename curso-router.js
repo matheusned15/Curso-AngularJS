75949235c415442df83dce78115441ccaf39c773
matheusned15 <matheusned15@gmail.com>
2021-04-28 23:51:39 -0300

    Capturando erros e orderBy

---
+++ curso-router.js
@@ -0,0 +1,12 @@
+angular.module('app').config(function($routeProvider){
+    $routeProvider
+    .when('/Cadastro/:idCli?', {
+        templateUrl: 'cadastro/cadastro.html',
+        controller: 'CadastroController as vm'
+    })
+    .otherwise({
+        templateUrl: 'home/home.html',
+        controller: 'HomeController as vm'
+    });
+
+})
